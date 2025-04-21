f9fec2b7fbf8597aaa71bde45a5773423a8b91bb
robbinn <robbinn@wwu.edu>
2025-04-21 15:35:22 -0700

    added in controler

---
+++ build/ros_gz_gazebo/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/robbinn/robotics/namespace/ros-lab-2/install/ros_gz_description/share/ros_gz_description/package.sh"
---
+++ ros_gz_app/dii_drive_controler.py
@@ -0,0 +1,61 @@
+#Author: Nolan Robbins
+#Date: April 21th 2025
+#Description:
+
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+from geometry_msgs.msg import Twist
+
+from sensor_msgs.msg import LaserScan
+
+
+class FallowWall(Node):
+    
+    def __init__(self):
+        super().__init__('fallow_wall')
+        self.publisher_ = self.create_publisher(Twist, 'diff_drive/cmd_vel', 10)
+        self.subscription = self.create_subscription( #sub
+            LaserScan,
+            'diff_drvie/scan',
+            self.listener_callback,
+            10)
+        
+        self.subscription #sub
+
+    
+
+    def listener_callback(self, msg):
+        # self.get_logger().info('Turtle Position: x=%.2f, y=%.2f, angle = %2f' % (position.x, position.y,position.theta))
+        distance = msg.range[0]
+        self.get_logger().info(f'Distance from laser: {distance:.2f}m')
+
+
+
+
+
+        msg = Twist()
+        msg.linear.x = 1.0
+        msg.angular.z = 0
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: linear.x={msg.linear.x}, angular.z={msg.angular.z}')
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    
+    wall_follower = FallowWall()
+
+    rclpy.spin(wall_follower)
+      
+    wall_follower.destroy_node()
+
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
